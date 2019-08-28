f787522507ce14b396a6296874b5b244aec1a291
douglas martins santos <douglas.santos@avecbrasil.com.br>
2019-08-28 18:25:47 -0300

    continue: Listando agenda do prestador

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,17 @@
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+    return res.json();
+  }
+}
+
+export default new ScheduleController();
