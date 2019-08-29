97d845490e0f49d1e908a37f325ef7434ebf769c
Douglas <douglasmartinssantoss@gmail.com>
2019-08-29 18:28:17 -0300

    chore: listando agenda do prestador

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,3 +1,6 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -10,7 +13,22 @@ class ScheduleController {
     if (!checkUserProvider) {
       return res.status(401).json({ error: 'User is not a provider' });
     }
-    return res.json();
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const Appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json({ Appointments });
   }
 }
 
