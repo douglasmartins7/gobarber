5e6a931ed6df7e6090c28c47292770a244006765
douglas martins santos <douglas.santos@avecbrasil.com.br>
2019-08-30 18:02:27 -0300

    chore: configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '1bac08b8d7e00b',
+    pass: '08cf495b39c523',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
